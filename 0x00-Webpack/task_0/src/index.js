a23c4f14cce7c6d2e68ddcd24ef1de0d3d151b4e
CharlyAnne <icharlieanne@gmail.com>
2023-07-24 19:25:29 +0100

    task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,11 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+  //Creates 3 new <p> elements
+  const prgh1 = $('<p>').text('Holberton Dashboard');
+  const prgh2 = $('<p>').text('Dashboard data for the students');
+  const prgh3 = $('<p>').text('Copyright - Holberton School');
+
+  //Append the new elements to the body of the page
+  $('body').append(prgh1, prgh2, prgh3);
+});
