dfa7b5aea34493fb46f5381a39e36547db5f7a8d
CharlyAnne <icharlieanne@gmail.com>
2023-07-25 00:44:37 +0100

    tasl 0: basics

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,6 @@
-const $ = require('jquery');
-
-const prgh1 = $('<p>').text('Holberton Dashboard');
-const prgh2 = $('<p>').text('Dashboard data for the students');
-const prgh3 = $('<p>').text('Copyright - Holberton School');
+const $ = require( "jquery" );
 
 //Append the new elements to the body of the page
-$('body').append(prgh1, prgh2, prgh3);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
