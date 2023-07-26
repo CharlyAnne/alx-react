bee6426699ec36653a6b54e06e678c12707f1abd
CharlyAnne <icharlieanne@gmail.com>
2023-07-26 01:54:56 +0100

    npm start

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,20 +1,20 @@
-import holberton_logo from './holberton-logo.jpg';
 import './App.css';
+import holberton_logo from './holberton-logo.jpg';
 
 function App() {
   return (
-  <>
-    <div className='App-header'>
-      <img src={hoberton_logo} alt='logo' />
-      <h1>School dashboard</h1>
-    </div>
-    <div className='App-body'>
-      <p>Login to access the full dashboard</p>
-    </div>
-    <div className='App-footer'>
-      <p>Copyright 2020 - Holberton school</p>
-    </div>
-  </>
+    <>
+      <div className="App-header">
+        <img src={holberton_logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </div>
+    </>
   );
 }
 
