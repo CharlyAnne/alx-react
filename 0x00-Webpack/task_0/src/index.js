ec2a9059213a1ee5785c1bdeb832a0ab0c82be0d
CharlyAnne <icharlieanne@gmail.com>
2023-07-27 00:59:37 +0100

    Updates

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,6 +0,0 @@
-const $ = require( "jquery" );
-
-//Append the new elements to the body of the page
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
