a4a9c1f50e88dda1fab883f0bdfa5610f98824c2
Charly <100117263+CharlyAnne@users.noreply.github.com>
2023-08-18 11:24:59 +0100

    Delete 0x06-React_immutable directory

--- 0x06-React_immutable/2-nested.js
+++
@@ -1,7 +0,0 @@
-import { fromJS } from 'immutable';
-
-export default function accessImmutableObject(object, array) {
-  const objs = fromJS(object);
-  const name = objs.getIn(array);
-  return name;
-}
