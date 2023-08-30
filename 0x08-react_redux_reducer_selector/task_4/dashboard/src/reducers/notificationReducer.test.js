a3a3df6a83a10b7b18e7fdcd88a94ea880a86422
Charly <icharlieanne@gmail.com>
2023-08-30 10:03:03 +0100

    redux task

---
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.test.js
@@ -0,0 +1,10 @@
+import notificationReducer, {
+  initialNotificationState,
+} from './notificationReducer';
+
+describe('courseReducer', function () {
+  it('initial state', function () {
+    const state = notificationReducer(undefined, {});
+    expect(state).toEqual(initialNotificationState);
+  });
+});
